2fdf955bed9cc25835109c57e1f5731145013e4b
Kevin Schanz <kschanz@imobile3.com>
2020-05-04 12:06:40 -0400

    Module 4 solution - prepare for sqlite3 dynamic content

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python3
 
+import sqlite3
+from flask import g
 from flask import Flask
 from flask import render_template
 
+PATH = 'db/jobs.sqlite'
+
 app: Flask = Flask(__name__)
+
+
+def open_connection() -> sqlite3.Connection:
+    connection = getattr(g, '_connection', None)
+    if connection is None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def execute_sql(sql: str, values: tuple = (),
+                commit: bool = False, single: bool = False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit is True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 
 @app.route('/')
